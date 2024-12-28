977a745deb5eed5ec678433859052ff62a5edb31
Siirko <alexander.yanovskyy@gmail.com>
2024-12-28 11:48:34 +0100

    vec3 to vec2 for tex_coords of Vertex

--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,11 +1,11 @@
 struct VertexInput {
     @location(0) position: vec3<f32>,
-    @location(1) tex_coords: vec3<f32>,
+    @location(1) tex_coords: vec2<f32>,
 };
 
 struct VertexOutput {
     @builtin(position) clip_position: vec4<f32>,
-    @location(0) tex_coords: vec3<f32>,
+    @location(0) tex_coords: vec2<f32>,
 };
 
 
--- src/state.rs
+++ src/state.rs
@@ -28,27 +28,27 @@ pub struct State<'a> {
 const VERTICES: &[Vertex] = &[
     Vertex {
         position: [-0.5, 0.5, 0.0],
-        tex_coords: [0.0, 0.0, 0.0],
+        tex_coords: [0.0, 0.0],
     },
     Vertex {
         position: [-0.5, -0.5, 0.0],
-        tex_coords: [0.0, 1.0, 0.0],
+        tex_coords: [0.0, 1.0],
     },
     Vertex {
         position: [0.5, -0.5, 0.0],
-        tex_coords: [1.0, 1.0, 0.0],
+        tex_coords: [1.0, 1.0],
     },
     Vertex {
         position: [-0.5, 0.5, 0.0],
-        tex_coords: [0.0, 0.0, 0.0],
+        tex_coords: [0.0, 0.0],
     },
     Vertex {
         position: [0.5, -0.5 , 0.0],
-        tex_coords: [1.0, 1.0, 0.0],
+        tex_coords: [1.0, 1.0],
     },
     Vertex {
         position: [0.5, 0.5, 0.0],
-        tex_coords: [1.0, 0.0, 0.0],
+        tex_coords: [1.0, 0.0],
     },
 ];
 
